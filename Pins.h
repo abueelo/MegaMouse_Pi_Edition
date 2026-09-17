14653df46475dfc82e6b2787ccb4df8103ef1d80
abueelo <grandpabenjamin1946@gmail.com>
2026-09-17 12:41:16 +0100

    update pin assignments

--- Pins.h
+++ Pins.h
@@ -9,20 +9,20 @@
 // Drivetrain - see Motor for what each pin does
 constexpr uint LeftMotorStepPin = 2;
 constexpr uint LeftMotorDirPin = 3;
-constexpr uint LeftMotorMsPin = 4;
+constexpr uint LeftMotorMsPin = 19;
 
 constexpr uint RightMotorStepPin = 5;
 constexpr uint RightMotorDirPin = 6;
-constexpr uint RightMotorMsPin = 7;
+constexpr uint RightMotorMsPin = 20;
 
 // IR sensors - all digital, active-high (HIGH = wall/reflection detected)
-constexpr uint FrontSensorPin = 8;
+constexpr uint FrontSensorPin = 10;
 
 // Left/right arrays, ordered inner (closest to center) to outer
-constexpr uint LeftSensorInnerPin = 9;
-constexpr uint LeftSensorMiddlePin = 10;
-constexpr uint LeftSensorOuterPin = 11;
+constexpr uint LeftSensorInnerPin = 4;
+constexpr uint LeftSensorMiddlePin = 5;
+constexpr uint LeftSensorOuterPin = 6;
 
-constexpr uint RightSensorInnerPin = 12;
-constexpr uint RightSensorMiddlePin = 13;
-constexpr uint RightSensorOuterPin = 14;
+constexpr uint RightSensorInnerPin = 7;
+constexpr uint RightSensorMiddlePin = 8;
+constexpr uint RightSensorOuterPin = 9;
